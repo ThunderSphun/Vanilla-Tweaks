dbb5d9caeeb52c0996cf9d8cb25d89599e78321b
ThunderSphun <manderijn2000@gmail.com>
2020-08-07 21:33:27 +0200

    made wrench work with redstone components

--- src/main/java/net/fabricmc/vanillaTweaks/WrenchItem.java
+++ src/main/java/net/fabricmc/vanillaTweaks/WrenchItem.java
@@ -7,15 +7,23 @@ import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.RayTraceContext;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WrenchItem extends Item {
+	private static final List<Direction> HEADINGS = new ArrayList<>(Properties.FACING.getValues());
+
 	public WrenchItem(Settings settings) {
 		super(settings);
 	}
@@ -25,28 +33,44 @@ public class WrenchItem extends Item {
 		BlockHitResult result = rayTrace(world, user, RayTraceContext.FluidHandling.NONE);
 		if (result.getType() == HitResult.Type.BLOCK) {
 			BlockState state = world.getBlockState(result.getBlockPos());
+
+			//checks if glazed terracotta is being targeted
 			if (Config.TERRACOTTA_WRENCH.isEnabled() && state.getBlock() instanceof GlazedTerracottaBlock) {
-				switch (state.get(HorizontalFacingBlock.FACING)) {
-					case SOUTH:
-						world.setBlockState(result.getBlockPos(), state.with(HorizontalFacingBlock.FACING,
-								user.isSneaking() ? Direction.EAST : Direction.WEST));
-						break;
-					case NORTH:
-						world.setBlockState(result.getBlockPos(), state.with(HorizontalFacingBlock.FACING,
-								user.isSneaking() ? Direction.WEST : Direction.EAST));
-						break;
-					case EAST:
-						world.setBlockState(result.getBlockPos(), state.with(HorizontalFacingBlock.FACING,
-								user.isSneaking() ? Direction.NORTH : Direction.SOUTH));
-						break;
-					case WEST:
-						world.setBlockState(result.getBlockPos(), state.with(HorizontalFacingBlock.FACING,
-								user.isSneaking() ? Direction.SOUTH : Direction.NORTH));
-						break;
-				}
+				this.swapState(world, state, result.getBlockPos(), HorizontalFacingBlock.FACING, user.isSneaking());
+				return TypedActionResult.success(user.getStackInHand(hand));
+
+				//checks if a configured redstone blockType is being targeted
+			} else if (Config.REDSTONE_WRENCH.isEnabled() && Config.REDSTONE_WRENCH.getIds()
+					.stream().anyMatch(e -> e.equals(state.getBlock()))) {
+				DirectionProperty property = (DirectionProperty) state.getProperties().stream()
+						.filter(p -> p instanceof DirectionProperty).findFirst().get();
+
+				this.swapState(world, state, result.getBlockPos(), property, user.isSneaking());
 				return TypedActionResult.success(user.getStackInHand(hand));
 			}
 		}
 		return TypedActionResult.pass(user.getStackInHand(hand));
+	}
+
+	private void swapState(World world, BlockState state, BlockPos pos, DirectionProperty property, boolean sneaking) {
+		List<Direction> dirList = new ArrayList<>(property.getValues());
+
+		int i = HEADINGS.indexOf(state.get(property)) + (sneaking ? -1 : 1);
+		if (i < 0) {
+			i = HEADINGS.size() - 1;
+		} else if (i >= HEADINGS.size()) {
+			i = 0;
+		}
+
+		while (!dirList.contains(HEADINGS.get(i))) {
+			i += sneaking ? -1 : 1;
+			if (i < 0) {
+				i = HEADINGS.size() - 1;
+			} else if (i >= HEADINGS.size()) {
+				i %= HEADINGS.size();
+			}
+		}
+
+		world.setBlockState(pos, state.with(property, HEADINGS.get(i)));
 	}
 }
