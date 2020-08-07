ea6542d0e70be39700cc6753deb474b08fde1474
ThunderSphun <manderijn2000@gmail.com>
2020-08-07 14:04:43 +0200

    made wrench work with terracotta

--- src/main/java/net/fabricmc/vanillaTweaks/WrenchItem.java
+++ src/main/java/net/fabricmc/vanillaTweaks/WrenchItem.java
@@ -1,6 +1,7 @@
 package net.fabricmc.vanillaTweaks;
 
 import net.fabricmc.vanillaTweaks.config.Config;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.GlazedTerracottaBlock;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.entity.player.PlayerEntity;
@@ -10,6 +11,7 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.RayTraceContext;
 import net.minecraft.world.World;
 
@@ -22,13 +24,29 @@ public class WrenchItem extends Item {
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
 		BlockHitResult result = rayTrace(world, user, RayTraceContext.FluidHandling.NONE);
 		if (result.getType() == HitResult.Type.BLOCK) {
-			System.out.println(result.getBlockPos());
-			if (Config.TERRACOTTA_WRENCH.isEnabled() &&
-					world.getBlockState(result.getBlockPos()).getBlock() instanceof GlazedTerracottaBlock) {
-				System.out.println("it is a terracotta");
-				world.getBlockState(result.getBlockPos()).cycle(HorizontalFacingBlock.FACING);
+			BlockState state = world.getBlockState(result.getBlockPos());
+			if (Config.TERRACOTTA_WRENCH.isEnabled() && state.getBlock() instanceof GlazedTerracottaBlock) {
+				switch (state.get(HorizontalFacingBlock.FACING)) {
+					case SOUTH:
+						world.setBlockState(result.getBlockPos(), state.with(HorizontalFacingBlock.FACING,
+								user.isSneaking() ? Direction.EAST : Direction.WEST));
+						break;
+					case NORTH:
+						world.setBlockState(result.getBlockPos(), state.with(HorizontalFacingBlock.FACING,
+								user.isSneaking() ? Direction.WEST : Direction.EAST));
+						break;
+					case EAST:
+						world.setBlockState(result.getBlockPos(), state.with(HorizontalFacingBlock.FACING,
+								user.isSneaking() ? Direction.NORTH : Direction.SOUTH));
+						break;
+					case WEST:
+						world.setBlockState(result.getBlockPos(), state.with(HorizontalFacingBlock.FACING,
+								user.isSneaking() ? Direction.SOUTH : Direction.NORTH));
+						break;
+				}
+				return TypedActionResult.success(user.getStackInHand(hand));
 			}
 		}
-		return super.use(world, user, hand);
+		return TypedActionResult.pass(user.getStackInHand(hand));
 	}
 }
